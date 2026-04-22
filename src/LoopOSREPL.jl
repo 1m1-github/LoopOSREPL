ef5c4b64073a2c4bacd630d399e4b84963becae1
1m1 <imi@1m1.io>
2026-04-22 13:55:17 -0600

    .

---
+++ src/LoopOSREPL.jl
@@ -0,0 +1,31 @@
+module LoopOSREPL
+
+using ReplMaker
+using LoopOS: listen, InputPeripheral
+import LoopOS: state
+import Base.take!
+
+struct REPLInput <: InputPeripheral
+    c::Channel{String}
+end
+
+take!(::REPLInput) = take!(REPL.c)
+state(::REPLInput) = "REPLModule.REPL"
+
+const REPL = REPLInput(Channel{String}(10))
+listen(REPL)
+
+repl_parse(s) = put!(REPL.c, string(strip("""$s""")))
+
+atreplinit() do _
+    ReplMaker.initrepl(
+        repl_parse,
+        prompt_text="> ",
+        prompt_color=:light_cyan,
+        start_key="\\C-g",
+        mode_name="God",
+    )
+    write(stdin.buffer, "\x07")
+end
+
+end
